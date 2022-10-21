bd9e0e4e30a7bf35a8b95f28bb8f9b6d73bae21d
Chiara Altobelli <chiara.j.altobelli@gmail.com>
2022-10-21 14:57:04 -0600

    Implrements #1

--- operations.test.js
+++ operations.test.js
@@ -2,7 +2,7 @@
 const {sum} = require('./operations')
 
 //TODO: uncomment the next line
-//const {subtract} = require('./operations')
+const {subtract} = require('./operations')
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3)
@@ -15,3 +15,15 @@ test('adds 0 + 4 to equal 4', () => {
 test('adds -1 + (-7) to equal -8', () => {
     expect(sum(-1, -7)).toBe(-8)
 });
+
+test('subtracts 5 from 9 to equal 4', () => {
+    expect(subtract(9, 5)).toBe(4);
+})
+
+test('subtracts -5 from 12 to equal 17', () => {
+    expect(subtract(12, -5)).toBe(17);
+})
+
+test('subtracts 2 from 10 to equal 8', () => {
+    expect(subtract(10, 2)).toBe(8);
+})
